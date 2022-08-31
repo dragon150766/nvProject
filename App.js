b1ee12057ff8409bccdc6856cbe13be2f0cfbeea
dragon150766 <dragon150766@hotmail.com>
2022-08-31 09:50:51 +0700

    08/31/2022

--- App.js
+++ App.js
@@ -71,7 +71,7 @@ function ThirdPage({ navigation}){
         }}/>
 
       </View>
-      <View style={style.textBottomStyle}>Thai-Nichi Institute of Technology</View>
+      <View style={style.textBottomStyle}>Thai-Nichi Institute of Technology.</View>
     </>
   );
 
