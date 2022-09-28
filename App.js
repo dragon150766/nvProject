63a54dff481c5460d5ca4de81ce9c7ff42e3f948
dragon150766 <dragon150766@hotmail.com>
2022-09-28 11:01:28 +0700

    9/28/2022

--- App.js
+++ App.js
@@ -1,47 +1,99 @@
-import { StyleSheet, Text, View, ActivityIndicator,FlatList } from 'react-native'
-import React , {useEffect, useState} from 'react'
+import * as React from "react";
+import { Button, View, Text, Image, StyleSheet } from "react-native";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import HomeScreenDrwaer from "./Screens/HomeScreenDrwaer";
+import { SafeAreaView } from "react-native-safe-area-context";
+import ProductScreen from "./Screens/ProductScreen";
 
-const App = () => {
 
-  const [isLoading,setLoading] = useState(true);
-  const [data,setData] = useState([]);
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "rgb(97, 201, 246)",
+    background: "rgb(300, 300, 300)",
+    card: "rgb(158, 177, 213)",
+    text: "rgb(144, 130, 250)",
+    border: "rgb(80, 138, 222)",
+    notification: "rgb(80, 138, 222)",
+  },
+};
 
-  const getMovie = async() =>{
-    try{
-
-      const response = await fetch('https://reactnative.dev/movies.json');
-      const json = await response.json();
-      setData(json.movies)
-
-    }catch(error){
-      alert(error.message);
-    }finally{
-      setLoading(false);
-    }
-  }
-
-  useEffect(() => {
-    getMovie();
-  },[])
-
+function FeedScreen({ navigation }) {
   return (
-    <View style={{flex:1,padding:20}}>
-      { isLoading ? <ActivityIndicator/>: (
-        <FlatList
-          data={data}
-          keyExtractor ={({ id }, index) => id}
-          renderItem={({item}) =>(
-            <Text>{item.title},{item.releaseYear}</Text>
-            
-          )}
-        />
-      )
-
-      }
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      {/* <Button
+        onPress={() => navigation.navigate("Notifications")}
+        title="Go to notifications"
+      /> */}
+      <Text>Feed Screen</Text>
+      <Button title="Open drawer" onPress={() => navigation.openDrawer()} />
+      <Button title="Yoggle drawer" onPress={() => navigation.toggleDrawer()} />
     </View>
-  )
+  );
 }
 
-export default App
 
-const styles = StyleSheet.create({})
+function CustomDrawerContent(props) {
+
+  return (
+   
+    <SafeAreaView style={{flex:1}}>
+      <Image source={require('./assets/react_logo.png')} style={styles.sideMenuProfileIcon}/>
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem
+        label="Close drawer"
+        onPress={() => props.navigation.closeDrawer()}
+      />
+    </DrawerContentScrollView>
+    </SafeAreaView>
+    
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  
+  return (
+    <Drawer.Navigator
+      useLegacyImplementation
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: "white",
+          width: 200,
+        },
+      }}
+    >
+      <Drawer.Screen name="Home" component={HomeScreenDrwaer} />
+      <Drawer.Screen name="Product" component={ProductScreen} />
+    </Drawer.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer theme={MyTheme}>
+      <MyDrawer />
+    </NavigationContainer>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  sideMenuProfileIcon: {
+  resizeMode: 'center',
+  width: 100,
+  height: 100,
+  borderRadius: 100 / 2,
+  alignSelf: 'center',
+  },
+})
